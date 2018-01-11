8c4fc92be9e6c4a91c778818a7537f7a83ae18e0
Andrea Li <bk20130048@my.swjtu.edu.cn>
2018-01-11 18:13:16 +0100

    Add

--- FIBONACCI/main.cpp
+++ FIBONACCI/main.cpp
@@ -3,9 +3,8 @@
 
 int main()
 {
-
 	long int n, r;
-	std::cout << "Fibonacci Series Calculator\n\nPlease insert the position in the series:\n\n";
+	std::cout << "Fibonacci Series Calculator\n\nPlease insert the position in the series:\n\n" << std::flush;
 	std::cin >> n;
 	r = fib(n);
 	std::cout << "\nThe " << n << "th number in the series is " << r << std::endl; 
